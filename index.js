28e7b9b0bd97af43b4d7d66d8796c021d8498c92
Rajat123-max <75437534+Rajat123-max@users.noreply.github.com>
2021-12-18 05:01:34 -0800

    commit

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const labels = [
   "Piyush",
   "Pranshul",
   "Rohit",
-  "Rajat"
 ];
 Promise.all([
   faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
@@ -75,7 +74,7 @@ async function start() {
       });
       drawBox.draw(canvas);
     });
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 9; i++) {
       if (
         document
           .getElementById("Attendees")
@@ -88,7 +87,7 @@ async function start() {
     document.getElementById("attendees").innerHTML = `Attendees : ${
       count - ucount
     } & unknown : ${ucount}`;
-    document.getElementById("absent").innerHTML = `Absent : ${10 - count} `;
+    document.getElementById("absent").innerHTML = `Absent : ${9 - count} `;
   });
 }
 
